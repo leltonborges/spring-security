4c015a74537800ff5ad3701a7489a6734169d6d9
Lelton Borges <leltondev@gmail.com>
2022-01-23 23:52:39 -0300

    Created Project

---
+++ src/main/java/com/web/jwt/webjwt/WebJwtApplication.java
@@ -0,0 +1,13 @@
+package com.web.jwt.webjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/jwt/webjwt/WebJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.jwt.webjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
